f35dea3a128203675e8c9940d44385e43da39740
Tess-Vanta <72697074+Tess-Vanta@users.noreply.github.com>
2025-05-29 10:30:32 +0530

    Updating circle confetti into hearts

--- index.js
+++ index.js
@@ -42,11 +42,9 @@ function startRomanticConfetti() {
     confetti.style.backgroundColor = "transparent";
     confetti.style.width = "10px";
     confetti.style.height = "10px";
-    confetti.style.clipPath =
-      "path('M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z')";
-    confetti.style.backgroundImage = `radial-gradient(circle, ${
+    confetti.innerHTML = `<svg width="10" height="10" viewBox="0 0 24 24" fill="${
       romanticColors[Math.floor(Math.random() * romanticColors.length)]
-    }, ${romanticColors[Math.floor(Math.random() * romanticColors.length)]})`;
+    }" xmlns="http://www.w3.org/2000/svg"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg>`;
     confettiContainer.appendChild(confetti);
   }
 
